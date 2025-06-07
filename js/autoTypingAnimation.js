6abc37bc86014e62981c47fb6c10a5dfd9566eb9
Sakshi Lokulwar <155177254+SakshiLokulwar@users.noreply.github.com>
2025-06-07 13:52:53 +0530

    Add files via upload

---
+++ js/autoTypingAnimation.js
@@ -0,0 +1,9 @@
+setTimeout(function () {
+  $(".about-info-inner").typed({
+    strings: [
+      "<span>Hi, I am Sakshi Lokulwar</span> ,  an aspiring Data Analyst with a strong academic foundation and a keen interest in uncovering insights from data. I’ve recently completed my Master of Computer Applications (MCA) from Dr. D. Y. Patil School of MCA, Lohegaon, Pune, where I developed a solid understanding of analytics, databases, and real-world problem-solving using data.During my academic journey, I explored tools like Power BI, SQL, Excel, and Python, which helped me analyze and visualize data effectively. I enjoy working with numbers, finding patterns, and turning raw data into meaningful stories that can guide business decisions.What excites me most about data analytics is the combination of logic and creativity it offers. I’m also interested in project management and have gained exposure to Agile methodologies, which help in managing tasks efficiently and collaboratively.Now, I'm eager to apply what I’ve learned in real-world settings, keep improving my skills, and contribute to data-driven projects that make an impact.",
+    ],
+    typeSpeed: 7,
+    contentType: "html",
+  });
+}, 100);
